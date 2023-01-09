745eb6d74618042b7ffe1e27cde4055e974735cc
Msouza472 <sdsouza472souza@gmail.com>
2023-01-09 09:55:06 -0300

    estruturas de repeticao

---
+++ trilha-java-basico/etrutura-repeticao/src/Teste.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+       String nome;
+
+       System.out.println("Nome: ");
+       nome = scan.next();
+    }
+  
+    
+}
