176585f12f82b0e8f2537b4fbff1947bfd8cf4c9
Msouza472 <sdsouza472souza@gmail.com>
2023-01-11 19:41:01 -0300

    exercicio-retornos

--- trilha-java-basico/exercicio-sobrecarga/src/Main.java
+++ trilha-java-basico/exercicio-sobrecarga/src/Main.java
@@ -3,11 +3,23 @@ public class Main {
     public static void main(String[] args) {
 
         // Quadrilátero
-        System.out.println("Exercício quadrilátero");
+      /*   System.out.println("Exercício quadrilátero");
         Quadrilatero.area(5);
         Quadrilatero.area(2d,5d);
         Quadrilatero.area(7,8,9);
         Quadrilatero.area(5f,5f);
+*/
 
+        // Retornos
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5,5);
+        System.out.println("Área do retângulo:" + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Área do trapézio:" + areaTrapezio);
     }
 }
