f4173d5e99cc54ab263c83a434d943e4b561be29
Msouza472 <sdsouza472souza@gmail.com>
2024-01-06 18:42:19 -0300

    versao 4

--- trilha-java-basico/controle-de-candidatos/src/candidatura/ProcessoSeletivo.java
+++ trilha-java-basico/controle-de-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,39 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo{
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-       imprimirSelecionados();
+       String [] candidatos = {"FELIPE", "MARCIA", "MATHEUS", "ANA", "FLAVIA", "CLEBER", "SONIA", "FRANCISCO", "CARLOS", "AUGUSTO"};
+       for(String candidato: candidatos){
+        entrandoEmContato(candidato);
+       }
+    }
+       static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando){
+                tentativasRealizadas++;
+            }else{
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        }while(continuarTentando && tentativasRealizadas < 3);{
+            if (atendeu) {
+                System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+            }else{
+                System.out.println("Não conseguimos contato com o candidato " + candidato + ", número máximo de tentativas realizadas " + tentativasRealizadas);
+            }
+        }
+       }
+    
+    //método auxiliar
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados(){
