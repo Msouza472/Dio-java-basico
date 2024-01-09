ec11fc220e3a0a5f887a8051deaac95fbf31b756
Msouza472 <116378836+Msouza472@users.noreply.github.com>
2024-01-09 15:24:11 -0300

    Update Contador.java

--- trilha-java-basico/DesafioControleFluxo/src/Contador.java
+++ trilha-java-basico/DesafioControleFluxo/src/Contador.java
@@ -9,5 +9,24 @@ public class Contador {
         
         System.out.println("Digite o parametro dois");
         int parametroDois = terminal.nextInt();
+
+        try{
+            //chamando o método contar
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm > parametroDois){
+           throw new ParametrosInvalidosException();
+
+        }else {
+           int contagem = parametroDois - parametroUm;
+            for (int i = 1; i < contagem + 1; i++) {
+                System.out.println("Imprimindo número " + i);
+            }
+        }
     }
 }
