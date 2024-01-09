e036f6a4e941d7f732e497620a6f2f5efa0bc3cb
Msouza472 <sdsouza472souza@gmail.com>
2024-01-09 10:52:27 -0300

    tratamentoExcecao

---
+++ trilha-java-basico/DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        
+        System.out.println("Digite o parametro um");
+        int parametroUm = terminal.nextInt();
+        
+        System.out.println("Digite o parametro dois");
+        int parametroDois = terminal.nextInt();
+    }
+}
