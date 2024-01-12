4aed19be52d6a50a76e09aff105182ab82ac9cea
Msouza472 <sdsouza472souza@gmail.com>
2024-01-12 07:58:20 -0300

    exercicios

---
+++ trilha-java-basico/OrientaçãoObjetos/src/School.java
@@ -0,0 +1,21 @@
+public class School {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name= "Jhon";
+        student1.age= 9;
+        student1.color= Color.FAIR;
+        student1.sex= Sex.MALE;
+
+        Student student2 = new Student();
+        student2.name= "Ana";
+        student2.age= 5;
+        student2.color= Color.FAIR;
+        student2.sex= Sex.FEMALE;
+
+        Student student3 = new Student();
+        student3.name= "Valentina";
+        student3.age= 10;
+        student3.color= Color.FAIR;
+        student3.sex= Sex.FEMALE;
+    }
+}
---
+++ trilha-java-basico/OrientaçãoObjetos/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    String name;
+    int age;
+    Color color;
+    Sex sex;
+
+    void eating(Food food){
+
+    }
+    void drinking(Eat eat){
+
+    }
+    void running(){
+        
+    }
+}
